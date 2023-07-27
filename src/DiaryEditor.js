7f6809b673e3454db5b689560a89a86f477a2351
Doyoonasdf <won1hana@naver.com>
2023-07-27 13:31:18 +0900

    state를  객체로 관리하기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,41 +1,38 @@
 import { useState } from 'react';
 
 const DiaryEditor = () => {
-	const [author, setAuthor] = useState('');
-	const [content, setContent] = useState('');
+	// const [author, setAuthor] = useState('');
+	// const [content, setContent] = useState('');
 
+	const [state, setState] = useState({
+		author: '',
+		content: '',
+	});
 	return (
 		<div className='DiaryEditor'>
 			<h2>오늘의 일기</h2>
 			<div>
 				<input
 					name='author'
-					value={author}
+					value={state.author}
 					onChange={(e) => {
-						// console.log(e.target.value);
-						// console.log(e.target.name);
-						setAuthor(e.target.value);
+						setState({
+							author: e.target.value,
+							content: state.content,
+						});
 					}}
 				/>
 			</div>
 			<div>
 				<textarea
-					value={content}
+					value={state.content}
 					onChange={(e) => {
-						setContent(e.target.value);
+						setState({
+							content: e.target.value,
+							author: state.author,
+						});
 					}}
 				/>
-				{/* <textarea
-					name=''
-					id=''
-					cols='30'
-					rows='10'
-					value={content}
-					onChange={(e) => {
-						setContent(e.target.value);
-						console.log(e.target.value);
-					}}
-				></textarea> */}
 			</div>
 		</div>
 	);
