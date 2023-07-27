16430abefdbdcb3076a1ed7c9afa304179af3305
Doyoonasdf <won1hana@naver.com>
2023-07-27 14:33:34 +0900

    상태변화 이벤트 핸들러로 합치기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,38 +1,29 @@
 import { useState } from 'react';
 
 const DiaryEditor = () => {
-	// const [author, setAuthor] = useState('');
-	// const [content, setContent] = useState('');
-
 	const [state, setState] = useState({
 		author: '',
 		content: '',
 	});
+
+	const handleChangeState = (e) => {
+		console.log(e.target.name);
+		console.log(e.target.value);
+
+		setState({
+			...state,
+			[e.target.name]: e.target.value,
+		});
+	};
+
 	return (
 		<div className='DiaryEditor'>
 			<h2>오늘의 일기</h2>
 			<div>
-				<input
-					name='author'
-					value={state.author}
-					onChange={(e) => {
-						setState({
-							author: e.target.value,
-							content: state.content,
-						});
-					}}
-				/>
+				<input name='author' value={state.author} onChange={handleChangeState} />
 			</div>
 			<div>
-				<textarea
-					value={state.content}
-					onChange={(e) => {
-						setState({
-							content: e.target.value,
-							author: state.author,
-						});
-					}}
-				/>
+				<textarea name='content' value={state.content} onChange={handleChangeState} />
 			</div>
 		</div>
 	);
