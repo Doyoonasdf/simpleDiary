fe148ef1c064dfd53471160bd5c21d6332a424e0
Doyoonasdf <won1hana@naver.com>
2023-08-01 16:12:50 +0900

    데이터를 배열로 저장해서 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 
+const DummyList = [
+	{
+		id: 1,
+		author: '이정환',
+		content: '하이 2',
+		emotion: 5,
+		created_date: new Date().getTime(),
+	},
+	{
+		id: 2,
+		author: '홍길동',
+		content: '하이 3',
+		emotion: 2,
+		created_date: new Date().getTime(),
+	},
+	{
+		id: 3,
+		author: '아무개',
+		content: '하이 4',
+		emotion: 1,
+		created_date: new Date().getTime(),
+	},
+];
 function App() {
 	return (
 		<div className='App'>
 			<DiaryEditor />
+			<DiaryList diaryList={DummyList} />
 		</div>
 	);
 }
